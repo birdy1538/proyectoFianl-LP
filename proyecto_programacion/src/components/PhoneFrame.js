7f2635757c4622d85f8a2f30de3115f2bdb74cf2
nefta2 <92494202+nefta2@users.noreply.github.com>
2024-11-05 14:10:49 -0500

    submit style changes

--- proyecto_programacion/src/components/PhoneFrame.js
+++ proyecto_programacion/src/components/PhoneFrame.js
@@ -5,26 +5,26 @@ import { useLocation } from 'react-router-dom';
 
 // Marco de pantalla de teléfono
 const PhoneFrame = styled(Container)({
-  width: '100%',
-  maxWidth: '480px',
-  height: '85vh',
-  aspectRatio: '9 / 16',
-  backgroundColor: '#f19f04',
-  boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.2)',
-  borderRadius: '16px',
-  overflow: 'hidden',
-  position: 'relative',
+	width: '100%',
+	maxWidth: '480px',
+	height: '85vh',
+	aspectRatio: '9 / 16',
+	backgroundColor: '#f1f1f1',
+	boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.2)',
+	borderRadius: '16px',
+	overflow: 'hidden',
+	position: 'relative',
 });
 
 const PhoneFrameComponent = ({ children }) => {
-  const location = useLocation();
+	const location = useLocation();
 
-  return (
-    <PhoneFrame>
-      {children}
-      <Navbar currentPath={location.pathname} />
-    </PhoneFrame>
-  );
+	return (
+		<PhoneFrame>
+			{children}
+			<Navbar currentPath={location.pathname} />
+		</PhoneFrame>
+	);
 };
 
 export default PhoneFrameComponent;
--- proyecto_programacion/src/pantallas/Login.js
+++ proyecto_programacion/src/pantallas/Login.js
@@ -5,86 +5,93 @@ import { useNavigate } from 'react-router-dom';
 import logo from '../imagenes/Recipelogo.png'; // Importar la imagen
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState(null);
-  const navigate = useNavigate();
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [error, setError] = useState(null);
+	const navigate = useNavigate();
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post('http://localhost:4000/login', { username, password });
-      if (response.data.message === "Inicio de sesión exitoso") {
-        localStorage.setItem('userId', response.data.userId);
-        navigate('/'); // Redirigir a la pantalla principal
-      }
-    } catch (err) {
-      setError('Invalid username or password. Please try again.');
-      console.error(err);
-    }
-  };
+	const handleLogin = async (e) => {
+		e.preventDefault();
+		try {
+			const response = await axios.post('http://localhost:4000/login', {
+				username,
+				password,
+			});
+			if (response.data.message === 'Inicio de sesión exitoso') {
+				localStorage.setItem('userId', response.data.userId);
+				navigate('/'); // Redirigir a la pantalla principal
+			}
+		} catch (err) {
+			setError('Invalid username or password. Please try again.');
+			console.error(err);
+		}
+	};
 
-  return (
-    <Box 
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        minHeight: '70vh',
-        padding: 3
-      }}
-    >
-      {/* Imagen centrada horizontalmente */}
-      <Box sx={{ mb: 3 }}>
-        <img src={logo} alt="Logo" style={{ maxWidth: '200px', width: '100%' }} />
-      </Box>
+	return (
+		<Box
+			sx={{
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+				justifyContent: 'center',
+				minHeight: '70vh',
+				padding: 3,
+			}}
+		>
+			{/* Imagen centrada horizontalmente */}
+			<Box sx={{ mb: 3 }}>
+				<img
+					src={logo}
+					alt="Logo"
+					style={{ maxWidth: '200px', width: '100%' }}
+				/>
+			</Box>
 
-      <Typography 
-        variant="h4" 
-        component="h1" 
-        gutterBottom
-        style={{ 
-          color: 'white', 
-          fontWeight: 'bold', 
-          fontFamily: 'Roboto, sans-serif',
-        }}
-      >
-        Login
-      </Typography>
-      {error && <Typography color="error">{error}</Typography>}
-      <form onSubmit={handleLogin} style={{ width: '100%', maxWidth: 400 }}>
-        <TextField
-          label="Username"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-        <TextField
-          label="Password"
-          type="password"
-          variant="outlined"
-          fullWidth
-          margin="normal"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <Button 
-          type="submit" 
-          variant="contained" 
-          color="primary" 
-          fullWidth
-          sx={{ marginTop: 2 }}
-        >
-          Iniciar Sesión
-        </Button>
-      </form>
-    </Box>
-  );
+			<Typography
+				variant="h4"
+				component="h1"
+				gutterBottom
+				style={{
+					color: '#c48304',
+					fontWeight: 'bold',
+					fontFamily: 'Roboto, sans-serif',
+				}}
+			>
+				Login
+			</Typography>
+			{error && <Typography color="error">{error}</Typography>}
+			<form onSubmit={handleLogin} style={{ width: '100%', maxWidth: 400 }}>
+				<TextField
+					label="Username"
+					variant="outlined"
+					fullWidth
+					margin="normal"
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+					required
+				/>
+				<TextField
+					label="Password"
+					type="password"
+					variant="outlined"
+					fullWidth
+					margin="normal"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					required
+				/>
+				<Button
+					type="submit"
+					variant="contained"
+					color="primary"
+					fullWidth
+					sx={{ marginTop: 2 }}
+				>
+					Iniciar Sesión
+				</Button>
+			</form>
+		</Box>
+	);
 };
 
 export default Login;
